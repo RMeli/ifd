e0d00837b4499960292fd7a1831dff15107d5bde
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2022-02-07 16:41:45 +0000

    add comments

--- ifd_get.py
+++ ifd_get.py
@@ -11,14 +11,13 @@ for i, r in df.iterrows():
     dirname = r.rec_pdb + "_" + r.lig_pdb
     os.makedirs(dirname, exist_ok=True)
 
-    print(dirname)
-
     rec_pdb_file = prody.fetchPDB(r.rec_pdb, chain=r.rec_chid, folder=dirname)
     lig_pdb_file = prody.fetchPDB(r.lig_pdb, folder=dirname)
 
     rec_pdb = prody.parsePDB(rec_pdb_file)
     lig_pdb = prody.parsePDB(lig_pdb_file)
 
+    # Compute structure alignment
     for cutoff in range(90, 0, -10):
         matches = prody.matchChains(
             rec_pdb,
@@ -32,15 +31,15 @@ for i, r in df.iterrows():
         if matches is not None:
             break
 
+    # Sort matches and get best one
     matches.sort(key=lambda t: t[2])
-
     match = matches[0]
 
+    # Superimpose structures
     t = prody.calcTransformation(match[1], match[0])
     t.apply(lig_pdb)
 
-    # Receptor for docking
-    # TODO: Selct metals
+    # Receptor for docking (including ions/metals)
     rec = rec_pdb.select(
         f"(protein and not hetatm and not water) or (ion within 5 of (hetatm and resname {r.rec_lig_id}))"
     )
