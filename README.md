e3ce45c7d148dca03ea091e8669f829ba7c1efd8
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2022-02-07 16:34:42 +0000

    scripts to build ifd dataset

---
+++ README.md
@@ -0,0 +1,14 @@
+# Induced Fit Docking Dataset
+
+Reconstruct (partial) data set used to validate Induced Fit Docking (IDF).
+
+> Sherman, W., Day, T., Jacobson, M. P., Friesner, R. A., & Farid, R. (2006). Novel procedure for modeling ligand/receptor induced fit effects. Journal of medicinal chemistry, 49(2), 534-553.
+
+## Notes
+
+Visually, the poses in structures `3pgh` and `1cx2` (chain `A`) does not appear to correspond to the ones in Fig. 6 and Fig. 7.
+
+## Dependencies
+
+* [pandas](https://pandas.pydata.org/)
+* [ProDy](http://prody.csb.pitt.edu/index.html)
---
+++ ifd_get.py
@@ -0,0 +1,58 @@
+import prody
+import pandas as pd
+
+import os
+
+df = pd.read_csv("IFDdata.csv")
+
+print(df)
+
+for i, r in df.iterrows():
+    dirname = r.rec_pdb + "_" + r.lig_pdb
+    os.makedirs(dirname, exist_ok=True)
+
+    print(dirname)
+
+    rec_pdb_file = prody.fetchPDB(r.rec_pdb, chain=r.rec_chid, folder=dirname)
+    lig_pdb_file = prody.fetchPDB(r.lig_pdb, folder=dirname)
+
+    rec_pdb = prody.parsePDB(rec_pdb_file)
+    lig_pdb = prody.parsePDB(lig_pdb_file)
+
+    for cutoff in range(90, 0, -10):
+        matches = prody.matchChains(
+            rec_pdb,
+            lig_pdb,
+            subset="backbone",
+            seqid=cutoff,
+            overlap=cutoff,
+            pwalign=True,
+        )
+
+        if matches is not None:
+            break
+
+    matches.sort(key=lambda t: t[2])
+
+    match = matches[0]
+
+    t = prody.calcTransformation(match[1], match[0])
+    t.apply(lig_pdb)
+
+    # Receptor for docking
+    # TODO: Selct metals
+    rec = rec_pdb.select(
+        f"(protein and not hetatm and not water) or (ion within 5 of (hetatm and resname {r.rec_lig_id}))"
+    )
+    prody.writePDB(os.path.join(dirname, f"{r.rec_pdb}.pdb"), rec)
+
+    # Cognate ligand of receptor for docking
+    # Needed to define the binding site
+    autoboxlig = rec_pdb.select(
+        f"hetatm and resname {r.rec_lig_id} and chain {r.rec_chid}"
+    )
+    prody.writePDB(os.path.join(dirname, f"{r.rec_pdb}_lig.pdb"), autoboxlig)
+
+    # Ligand for docking
+    lig = lig_pdb.select(f"hetatm and resname {r.lig_id} and chain {r.lig_chid}")
+    prody.writePDB(os.path.join(dirname, f"{r.lig_pdb}_lig.pdb"), lig)
